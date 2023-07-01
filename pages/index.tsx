f47f8cf3be0289e48d10e9ba53c8aeaa9a8e4c72
Wei Kang <chongweikang5@gmail.com>
2023-07-01 14:08:08 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,30 +5,27 @@ import test from '../images/banner.png';
 import { createStyles, rem } from '@mantine/core';
 
 const useStyles = createStyles((theme) => ({
-  
-  img:{
-    padding:'50px'
-  }
+  img: {
+    padding: '50px',
+  },
 }));
 
 export default function HomePage() {
-  const {classes} = useStyles();
+  const { classes } = useStyles();
 
   return (
-    <Layout>
-      <div style={{textAlign:'center', marginTop:'-60px'}}>
-        <h1 style={{ fontSize: '65px', fontWeight: 'bold'}}>
-          Welcome to SummAIze
-        </h1>
-        <p style={{ fontSize: '20px', paddingLeft:"150px", paddingRight:"150px"}}>
-          We are a group of students looking to streamline the process of absorbing information. 
-          From summarizing pdf lectures notes to video lectures, our AI powered note collation system
-          helps students from all over the world condense high volumes of study materials in a matter
-          of seconds.
+    <>
+      <div style={{ textAlign: 'center', marginTop: '-60px' }}>
+        <h1 style={{ fontSize: '65px', fontWeight: 'bold' }}>Welcome to SummAIze</h1>
+        <p style={{ fontSize: '20px', paddingLeft: '150px', paddingRight: '150px' }}>
+          We are a group of students looking to streamline the process of absorbing information.
+          From summarizing pdf lectures notes to video lectures, our AI powered note collation
+          system helps students from all over the world condense high volumes of study materials in
+          a matter of seconds.
         </p>
-        <Image className={classes.img} src = {test.src}/>
+        <Image className={classes.img} src={test.src} />
       </div>
-    <ArticleSection/>
-    </Layout>
+      <ArticleSection />
+    </>
   );
 }
