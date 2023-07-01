f3e88977d0983202b6daf1cb41abd90270b2ef52
Kane <kanetan4@gmail.com>
2023-07-01 12:00:13 +0800

    HomePageBasic

---
+++ components/SearchBar.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useRef } from 'react';
+import { createStyles, rem } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+    searchBar: {
+      display: 'flex',
+      alignItems: 'center',
+      borderColor:'white',
+      borderWidth:'10px',
+    },
+    searchInput: {
+      padding: rem(8),
+      marginRight: rem(8),
+    },
+    searchButton: {
+      padding: `${rem(8)} ${rem(16)}`,
+      backgroundColor: theme.colors.blue[500],
+      color: theme.white,
+      border: 'none',
+      cursor: 'pointer',
+    },
+  }));
+
+  const SearchBar = ({ onSearch }: { onSearch: (query: string) => void }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const {classes} = useStyles();
+  
+    const handleSearch = () => {
+      const query = inputRef.current?.value || '';
+      onSearch(query);
+    };
+  
+    return (
+      <div className={classes.searchBar}>
+        <input
+          type="text"
+          className={classes.searchInput}
+          ref={inputRef}
+        />
+        <button
+          className={classes.searchButton}
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
+    );
+  };
+  
+  export default SearchBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import Layout from '../components/navigation/Layout';
+import ArticleSection from '../components/ArticleSection';
 
 export default function HomePage() {
   return (
     <Layout>
-      <div>Hello world</div>
       <ColorSchemeToggle />
+      <div style={{ padding: '20px', textAlign:'center' }}>
+        <h1 style={{ fontSize: '45px', fontWeight: 'bold'}}>
+          Welcome to SummAIze
+        </h1>
+        <p style={{ fontSize: '20px', paddingLeft:"150px", paddingRight:"150px"}}>
+          We are a group of students looking to streamline the process of absorbing information. 
+          From summarizing pdf lectures notes to video lectures, our AI powered note collation system
+          helps students from all over the world condense high volumes of study materials in a matter
+          of seconds.
+        </p>
+      </div>
+    <ArticleSection/>
     </Layout>
   );
 }
