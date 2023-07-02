708d8e55ce4fa3caa934e8790c38498de83c0bdc
Kane <kanetan4@gmail.com>
2023-07-02 23:42:43 +0800

    Update Article Section

--- components/ArticleSection.tsx
+++ components/ArticleSection.tsx
@@ -1,22 +1,17 @@
 import ArticleCard from '../components/ArticleCard';
 import SearchBar from '../components/SearchBar';
+import { useEffect, useState } from 'react';
 
 import {
     Card,
     createStyles,
   } from '@mantine/core';
 
-const articleData = {
-    image: "https://i.imgur.com/Cij5vdL.png",
-    link: "https://mantine.dev/",
-    title: "Resident Evil Village review",
-    rating: "outstanding",
-    description: "Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires.",
-    author: {
-      name: "Bill Wormeater",
-      image: "https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80"
-    }
-  };
+type Note = {
+    _id: string;
+    filename: string;
+    length: number;
+};
 
 const useStyles = createStyles((theme) => ({
     card: {
@@ -41,36 +36,37 @@ export default function ArticleSection() {
         console.log('Search query:', query);
     };
 
+    const [notes, setNotes] = useState<Note[]>([]);
+
+    useEffect(() => {
+        const getNotes = async () => {
+            const response = await fetch('/api/allFiles');
+            const data = await response.json();
+            console.log(data);
+            setNotes(Array.isArray(data) ? data : []);
+        };
+        getNotes();
+    }, []);
+
     return (
         <div className={classes.header}>
             <h2 >Search through our database of over 10 thousand notes!</h2>
             <SearchBar onSearch={handleSearch} />
             <Card className= {classes.card} >
-                
-                <ArticleCard
-                    image={articleData.image}
-                    link={articleData.link}
-                    title={articleData.title}
-                    description={articleData.description}
-                    rating={articleData.rating}
-                    author={articleData.author}
-                />
-                <ArticleCard
-                    image={articleData.image}
-                    link={articleData.link}
-                    title={articleData.title}
-                    description={articleData.description}
-                    rating={articleData.rating}
-                    author={articleData.author}
-                />
-                <ArticleCard
-                    image={articleData.image}
-                    link={articleData.link}
-                    title={articleData.title}
-                    description={articleData.description}
-                    rating={articleData.rating}
-                    author={articleData.author}
-                />
+                {notes.map((note, index) => (
+                    <ArticleCard
+                        key={index}
+                        image="https://i.imgur.com/Cij5vdL.png"  
+                        link={`/api/files/${note._id}`}
+                        title={note.filename}
+                        description={`${note.length} bytes`}    
+                        rating="outstanding"                    
+                        author={{                               
+                        name: "Bill Wormeater",
+                        image: "https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80"
+                        }}
+                    />
+                    ))}
             </Card>
         </div>
     )
