78597c08cd87075f979aec291e9eb8832fc9b28f
Sua Qi Rong <qirong36@gmail.com>
2023-07-03 01:23:52 +0800

    Added unique user notes

--- pages/all-notes/index.tsx
+++ pages/all-notes/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import ArticleCard from '../../components/ArticleCard';
-import { Card, createStyles } from '@mantine/core';
+import { Card, createStyles, Center } from '@mantine/core';
+import { useSession } from 'next-auth/react';
 
 type Note = {
   _id: string;
@@ -11,7 +11,7 @@ type Note = {
 
 const useStyles = createStyles((theme) => ({
   card: {
-    marginTop: "50px",
+    marginTop: '50px',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gap: theme.spacing.md,
@@ -22,12 +22,13 @@ const useStyles = createStyles((theme) => ({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 }));
 
 export default function AllNotesPage() {
   const [notes, setNotes] = useState<Note[]>([]);
   const { classes } = useStyles();
+  const { data: session } = useSession();
 
   useEffect(() => {
     const getNotes = async () => {
@@ -37,27 +38,36 @@ export default function AllNotesPage() {
     };
 
     getNotes();
-  }, []);
+  }, [session]);
 
   return (
-    <div className={classes.header}>
-      <h1>All Notes</h1>
-      <Card className={classes.card}>
-        {notes.map((note, index) => (
-          <ArticleCard
-            key={index}
-            image="https://i.imgur.com/Cij5vdL.png"  
-            link={`/api/files/${note._id}`}
-            title={note.filename}
-            description={`${note.length} bytes`}    
-            rating="outstanding"                    
-            author={{                               
-              name: "Bill Wormeater",
-              image: "https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80"
-            }}
-          />
-        ))}
-      </Card>
-    </div>
+    <>
+      {session ? (
+        <div className={classes.header}>
+          <h1>All Notes</h1>
+          <Card className={classes.card}>
+            {notes.map((note, index) => (
+              <ArticleCard
+                key={index}
+                image="https://i.imgur.com/Cij5vdL.png"
+                link={`/api/files/${note._id}`}
+                title={note.filename}
+                description={`${note.length} bytes`}
+                rating="outstanding"
+                author={{
+                  name: 'Bill Wormeater',
+                  image:
+                    'https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80',
+                }}
+              />
+            ))}
+          </Card>
+        </div>
+      ) : (
+        <Center maw={400} h={100} mx="auto">
+          <strong>Sign in to view your notes</strong>
+        </Center>
+      )}
+    </>
   );
 }
