ef7789ed7c3582309c633a893fbb54dc2c86f0ff
Kane <kanetan4@gmail.com>
2023-07-04 02:05:32 +0800

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,19 +5,32 @@ import { createStyles, rem } from '@mantine/core';
 
 const useStyles = createStyles((theme) => ({
     searchBar: {
+      width:'100%',
       display: 'flex',
       alignItems: 'center',
+      borderRadius:'5px'
     },
     searchInput: {
+      width:'500px',
       padding: rem(8),
       marginRight: rem(8),
+      borderRadius:'10px',
+      borderColor:'white',
     },
     searchButton: {
       padding: `${rem(8)} ${rem(16)}`,
-      color: theme.white,
+      // color: theme.white,
       border: 'none',
       cursor: 'pointer',
+      borderRadius:'10px',
+      fontWeight:'bold',
+      fontSize:'20px',
     },
+    bar: {
+      // width:'100%',
+      borderColor:'white',
+      borderWidth:'10px',
+    }
 }));
 
   const SearchBar = ({ onSearch }: { onSearch: (query: string) => void }) => {
@@ -30,18 +43,21 @@ const useStyles = createStyles((theme) => ({
     };
   
     return (
-      <div className={classes.searchBar}>
-        <input
-          type="text"
-          className={classes.searchInput}
-          ref={inputRef}
-        />
-        <button
-          className={classes.searchButton}
-          onClick={handleSearch}
-        >
-          Search
-        </button>
+      <div className={classes.bar}>
+
+        <div className={classes.searchBar}>
+          <input
+            type="text"
+            className={classes.searchInput}
+            ref={inputRef}
+          />
+          <button
+            className={classes.searchButton}
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
       </div>
     );
   };
