a4f96b3667eb3824ca64231fd83060a74054e1ef
Sua Qi Rong <qirong36@gmail.com>
2023-07-09 19:36:40 +0800

    Fixed bookmarks

--- pages/api/allBookmarks.ts
+++ pages/api/allBookmarks.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+/*import type { NextApiRequest, NextApiResponse } from 'next';
 import { MongoClient, Db, ObjectId, GridFSBucket } from 'mongodb';
 import connectToUserDB from '../../database/userConn';
 import connectToAuthDB from '../../database/authConn';
@@ -67,6 +67,54 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       } else {
         res.status(404).json({ error: 'User not found' });
       }
+    } catch (error) {
+      res.status(500).json({ error: 'Error connecting to the database' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}*/
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { GridFSBucket } from 'mongodb';
+import connectToAuthDB from '../../database/authConn';
+import connectToUserDB from '../../database/userConn';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      const email = req.query.email;
+      const dbFiles = await connectToAuthDB();
+      const dbUser = await connectToUserDB();
+      const bucket = new GridFSBucket(dbFiles);
+      const user = await dbUser.collection('users').findOne({ email: email });
+
+      // Create an array to store the file data
+      let filesData: any = [];
+
+      // Create a stream and use it to read filenames from GridFS
+      const stream = bucket.find({}).sort({ _id: -1 }).stream();
+
+      stream.on('data', (doc) => {
+        if (user?.bookmarks.includes(doc._id.toString())) {
+          // Push each file data to the array
+          filesData.push({
+            filename: doc.filename,
+            length: doc.length,
+            userName: doc.metadata.userName,
+            title: doc.metadata.title,
+            _id: doc._id.toString(),
+          });
+        }
+      });
+
+      stream.on('error', (error) => {
+        res.status(500).json({ error: 'Error reading files' });
+      });
+
+      stream.on('end', () => {
+        // When the stream is finished, return the files data
+        res.json(filesData);
+      });
     } catch (error) {
       res.status(500).json({ error: 'Error connecting to the database' });
     }
