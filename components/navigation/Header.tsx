0c06b453e29dddf7508acd414cdf7c2f39475a57
Wei Kang <chongweikang5@gmail.com>
2023-07-10 23:35:28 +0800

    Update Header.tsx

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -44,20 +44,20 @@ const useStyles = createStyles((theme) => ({
   link: {
     display: 'flex',
     alignItems: 'center',
-    height: '100%',
-    paddingLeft: theme.spacing.md,
-    paddingRight: theme.spacing.md,
+    height: 'auto', // Adjust this
+    padding: theme.spacing.lg,
     textDecoration: 'none',
     color: theme.colorScheme === 'dark' ? theme.white : theme.black,
     fontWeight: 500,
     fontSize: theme.fontSizes.sm,
 
     [theme.fn.smallerThan('sm')]: {
-      height: rem(42),
+      height: rem(42), // You might want to adjust this as well
       display: 'flex',
       alignItems: 'center',
       width: '100%',
     },
+
 
     ...theme.fn.hover({
       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
